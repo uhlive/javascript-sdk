7409207fe326e2e75e2a33712324c6c6e9e723fe
Robin Monteil <r.monteil@allo-media.fr>
2022-01-14 18:11:32 +0100

    Use `npm version xxx` instead of current code

--- release.sh
+++ release.sh
@@ -32,18 +32,11 @@ npm run doc
 echo "### Merge master to production"
 git merge master production
 
-OLD_VERSION=$(cat VERSION)
-sed -i "s/$OLD_VERSION/$1/" package.json
-sed -i "s/$OLD_VERSION/$1/" package-lock.json
-
 echo "### Update VERSION to $1"
 echo "$1" > VERSION
 
-VERSION=$(cat VERSION)
-
 echo "### Commit and tag release"
-git commit -am "Release v$VERSION"
-git tag "v$VERSION"
+npm version "$1"
 
 echo "### Push to remote"
 git push && git push --tags
@@ -55,9 +48,7 @@ git merge production master
 echo "### Set development version"
 DEV_VERSION="$VERSION-dev"
 echo "$DEV_VERSION" > VERSION
-sed -i -E 's/(^  "version": ")'$VERSION'(",)$/\1'$DEV_VERSION'\2/' package.json
-sed -i -E 's/(^  "version": ")'$VERSION'(",)$/\1'$DEV_VERSION'\2/' package-lock.json
-git commit -am "Set development version to $DEV_VERSION"
+npm version "$DEV_VERSION"
 git push
 
 echo "########## SUCCESS! ##########"
