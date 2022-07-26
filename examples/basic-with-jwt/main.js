76777effc3cba3c388bd52a9f4416e42994c7284
Robin Monteil <r.monteil@allo-media.fr>
2022-07-26 17:12:55 +0200

    Fix config files

--- examples/basic-with-jwt/main.js
+++ examples/basic-with-jwt/main.js
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js"
 import { Uhlive } from "@uhlive/javascript-sdk";
-import { uhliveConfig, keycloakConfig} from "./settings.js";
+import { keycloakConfig} from "./settings.js";
 
 window.onload = () => {
     const keycloak = new Keycloak(keycloakConfig);
@@ -11,7 +11,6 @@ window.onload = () => {
             const uhlive = new Uhlive({
                 identifier: keycloak.tokenParsed.azp,
                 jwtToken: keycloak.token,
-                url: uhliveConfig.url
               });
             uhlive
                 .connect()
